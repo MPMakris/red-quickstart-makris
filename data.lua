19430dc3eac9cde80553d5b6e0f13ed33037c446
Michael M <mpmakris@gmail.com>
2026-03-25 19:32:38 -0700

    Update data.lua

--- data.lua
+++ data.lua
@@ -272,7 +272,7 @@ data:extend({
     stack_size = 1,
     infinite = true,
     equipment_grid = 'rr-equipment-grid',
-    inventory_size_bonus = 300,
+    inventory_size_bonus = 400,
     provides_flight = feature_flags.space_travel,
     open_sound = { filename = '__base__/sound/armor-open.ogg', volume = 1 },
     close_sound = { filename = '__base__/sound/armor-close.ogg', volume = 1 },
